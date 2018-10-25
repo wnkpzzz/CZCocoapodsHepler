e7f807a77f7b1f9f955c2a31e131ab94b6623413
Hans3D <hans3d@Hans3DdeMac-mini.local>
2018-10-25 16:00:35 +0800

    1.0

---
+++ CZTestLibDemo/CZTestLibDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CZTestLibDemo
+//
+//  Created by Hans3D on 2018/10/25.
+//  Copyright © 2018年 wnkpzzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CZTestLibDemo/CZTestLibDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CZTestLibDemo
+//
+//  Created by Hans3D on 2018/10/25.
+//  Copyright © 2018年 wnkpzzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
